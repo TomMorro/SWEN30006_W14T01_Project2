5aa7405cf77202873f0462c8bceda0ff4f56f54d
dynnhw <dyno.wibowo@gmail.com>
2021-05-23 10:26:50 +1000

    initial implementation for CompositeShowScoringStrategy

--- Cribbage/src/cribbage/scoring/CompositeShowScoringStrategy.java
+++ Cribbage/src/cribbage/scoring/CompositeShowScoringStrategy.java
@@ -1,4 +1,42 @@
 package cribbage.scoring;
 
-public class CompositeShowScoringStrategy {
+import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
+
+
+
+public class CompositeShowScoringStrategy extends CompositeScoringStrategy{
+    public CompositeShowScoringStrategy(){
+        super();
+        super.add(new RunScoringStrategy());
+        super.add(new PairScoringStrategy());
+        //super.add(new FlushScoringStrategy());
+        super.add(new JackScoringStrategy());
+    }
+
+    @Override
+    public int getPoints(Hand cards) {
+        int totalScore = 0;
+
+
+        // get starter card located at the first index
+        Card starter = cards.getFirst();
+
+        // remove starter before sorting cards
+        cards.removeFirst(false);
+
+        // sort
+        cards.sort(cards.getSortType(), false);
+
+        // a bit of a hacky way to insert the starter at the first index again
+        cards.reverse(false);
+        cards.insert(starter, false);
+        cards.reverse(false);
+
+        for (IScoringStrategy strategy : scoringStrategies) {
+            totalScore += strategy.getPoints(cards);
+        }
+
+        return totalScore;
+    }
 }
