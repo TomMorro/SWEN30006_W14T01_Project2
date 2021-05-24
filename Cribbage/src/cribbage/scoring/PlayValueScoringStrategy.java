1c06c63a223b2d67b125eac44192bd756f6a5347
tdmorrison <tdmorrison@student.unimelb.edu.au>
2021-05-24 14:27:37 +1000

    Fixed play scoring of Value

--- Cribbage/src/cribbage/scoring/PlayValueScoringStrategy.java
+++ Cribbage/src/cribbage/scoring/PlayValueScoringStrategy.java
@@ -1,12 +1,21 @@
 package cribbage.scoring;
 
 import ch.aplu.jcardgame.Hand;
+import cribbage.Cribbage;
+import cribbage.Cribbage.Rank;
+import ch.aplu.jcardgame.Card;
 
 public class PlayValueScoringStrategy extends ValueScoringStrategy{
 
 	@Override
 	public int getPoints(Hand cards) {
-		if (cards.getScore() == fifteen || cards.getScore() == thirtyone) {
+		int total = 0;
+		
+		for (Card card : cards.getCardList()) {
+			Rank rank = (Cribbage.Rank) card.getRank();
+			total += rank.value;
+		}
+		if (total == fifteen || total == thirtyone) {
 			return value;
 		}
 		return 0;
